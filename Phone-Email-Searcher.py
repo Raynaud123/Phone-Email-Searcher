5168cb2f916839b787c4c280d9eb7b3a9c838403
raynaud123 <raynaud.cornille@gmail.com>
2020-11-13 16:49:54 +0100

    Final code
    Finished project

--- Phone-Email-Searcher.py
+++ Phone-Email-Searcher.py
@@ -3,9 +3,9 @@
 
 import pyperclip, re
 #Making the regex for the phonenumbers and for the email
-phone =re.compile(r'''(
-((\+\d\d) | (00\d\d))?        #Land Code or Without (Belgium)
-( )?                    #Potential Space after Land code
+phone=re.compile(r'''(
+((\+\d\d)|(00\d\d))?        #Land Code or Without (Belgium)
+(\s)?                    #Potential Space after Land code
 ((0)?\d{3})             #First 3 digits with or without 0
 (\s|-|\.)?              #Potential whitespace or other character
 (\d{2})                 #2 digits
@@ -13,9 +13,10 @@ phone =re.compile(r'''(
 (\d{2})                 #2 digits
 (\s|-|\.)?              #Potential whitespace or other character
 (\d{2})                 #2 digits
-)''', re.VERBOSE) 
+)''',re.VERBOSE) 
 
 email=re.compile(r'''(
+    (\s)?
     [a-zA-z0-9._%+-]+            #First Words
     @                            #@ Symbol   
     [a-zA-z0-9._%+-]+            #Domain
@@ -28,10 +29,13 @@ phoneMatches=[]
 for groups in email.findall(text):
     emailMatches.append(groups[0])
 for groups in phone.findall(text):
-    if groups[8]==None:
-        ' '.join([groups[1],groups[3],groups[5],groups[7]])
-    elif groups[1]==' ':
-        ' '.join([groups[2],groups[4],groups[6],groups[8]])
-    elif groups[2]==' ':
-        ' '.join([groups[1],groups[3],groups[5],groups[7],groups[9]])
+    if groups[1]!=None:
+        phoneMatches.append(' '.join([groups[1],groups[5],groups[8],groups[10],groups[12]]))
+    else:
+        phoneMatches.append(' '.join([groups[5],groups[8],groups[10],groups[12]]))
+    #control mechanism
+#Pasting all the Matches onto the clipboard 
+pyperclip.copy("The emails are: "+ "\n" +"\n".join(emailMatches) +  "\n" + "The phoneNumbers are: " + "\n" + "\n".join(phoneMatches))
+
+  
 
